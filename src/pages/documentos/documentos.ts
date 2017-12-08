fe57860d0643b2798bd91238d09b066d85301e25
DESKTOP-F36ELQB\estal <stalyn797@gmail.com>
2017-12-08 01:07:04 -0500

    cambios

--- src/pages/documentos/documentos.ts
+++ src/pages/documentos/documentos.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { firebaseService } from '../../services/firebase.service';
-import { download } from './../../models/download';
+
 /**
  * Generated class for the DocumentosPage page.
  *
